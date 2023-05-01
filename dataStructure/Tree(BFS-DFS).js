01639ecba81ac4f9e76f48440544ab87f1a483b3
“yajureDogra” <yluck17@gmail.com>
2023-05-01 14:28:23 +0530

    tree DFS and BFS algo

---
+++ dataStructure/Tree(BFS-DFS).js
@@ -0,0 +1,40 @@
+export class Node {
+    constructor(data) {
+        this.data = data
+        this.children = []
+    }
+
+    add(data) {
+        this.children.push(new Node(data))
+    }
+
+    remove(data) {
+        this.children = this.children.filter(node => node.data !== data)
+    }
+}
+
+export class Tree {
+
+    constructor() {
+        this.root = null // head of tree
+    }
+
+    traverseBF(fn) {
+        const arr = [this.root]
+        while (arr.length) {
+            const node = arr.shift()
+            arr.push(...node.children) // to add items at last of array
+            fn(node)
+        }
+    }
+
+    traverseDF(fn) {
+        const arr = [this.root]
+        while (arr.length) {
+            const node = arr.shift()
+            arr.unshift(...node.children) // to add items at front of array
+            fn(node)
+        }
+    }
+
+}
